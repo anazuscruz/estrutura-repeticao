19f64644c4588586e1e2eea5bdbfbbb94c6fe98d
Suzana <anazus.cruz@gmail.com>
2024-05-20 14:00:36 -0300

    for each

---
+++ estrututra-repetição/src/forEach.java
@@ -0,0 +1,13 @@
+public class forEach {
+public static void main(String[] args) {
+    String alunos [] = {"Mario", "Marcia","Danilo","Pakita"};
+
+    for(String aluno : alunos){
+        System.out.println("Nome do aluno é " + aluno);
+
+    }
+  
+  }
+
+}
+ 
