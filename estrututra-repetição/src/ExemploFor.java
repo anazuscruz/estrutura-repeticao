662fcfd864dc8c6d4df99c534bccbe29b671147b
Suzana <anazus.cruz@gmail.com>
2024-05-20 13:25:07 -0300

    exemplo for

---
+++ estrututra-repetição/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        for (int carros = 1; 
+        carros <=20; 
+        carros ++){
+            System.out.println("Quantidade de carros " + carros);
+
+        }
+    }
+}
